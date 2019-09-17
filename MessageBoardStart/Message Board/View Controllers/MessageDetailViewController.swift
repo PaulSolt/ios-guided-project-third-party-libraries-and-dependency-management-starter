10719dae11929721de6da3710b1a8b5c2da3acce
Paul Solt <PaulSolt@gmail.com>
2019-09-17 15:30:50 -0400

    Refactoring View Controllers

--- MessageBoardStart/Message Board/View Controllers/MessageDetailViewController.swift
+++ MessageBoardStart/Message Board/View Controllers/MessageDetailViewController.swift
@@ -2,35 +2,22 @@
 //  MessageDetailViewController.swift
 //  Message Board
 //
-//  Created by Spencer Curtis on 8/7/18.
-//  Copyright © 2018 Lambda School. All rights reserved.
+//  Created by Paul Solt on 9/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
 //
 
 import UIKit
 
 class MessageDetailViewController: UIViewController {
-
-    // MARK: - Properties
     
     var messageThreadController: MessageThreadController?
     var messageThread: MessageThread?
     
-    @IBOutlet weak var senderNameTextField: UITextField!
-    @IBOutlet weak var messageTextView: UITextView!
-
-    // MARK: - Actions
     
-    @IBAction func sendMessage(_ sender: Any) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        guard let senderName = senderNameTextField.text,
-            let messageText = messageTextView.text,
-            let messageThread = messageThread else { return }
         
-        messageThreadController?.createMessage(in: messageThread, withText: messageText, sender: senderName, completion: {
-            DispatchQueue.main.async {
-                self.navigationController?.popViewController(animated: true)
-            }
-        })
     }
-
 }
+
